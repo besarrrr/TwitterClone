70216012d2b12ba19feee5effe3038029a910d55
Cesar Gonzalez <gcesar13@yahoo.com>
2023-01-04 20:19:38 -0700

    connect to mongo db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("sucecess");
+    })
+    .catch((err) => {throw err})
+};
 
 app.listen(3001, () => {
+ connect();
  console.log("Nice")
-
 });   
