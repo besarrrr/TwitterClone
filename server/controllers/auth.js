f9355a6cc7ace8f36ab8cef9e42ffb7aa61205b9
Cesar Gonzalez <gcesar13@yahoo.com>
2023-01-07 09:10:47 -0700

    signin

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,7 +31,16 @@ export const signin = async(req, res, next) => {
         const user = await User.findOne({ username: req.body.username})
         if(!user) return next(handleError(404, "User not found"));
 
-        const isCorrect = await bcrypt.compare(req.body.password, user.password)
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+        if(!isCorrect) return next(handleError(400, "Wrong Password"));
+
+        const token = jwt.sign({id: user._id}, process.env.JWT);
+
+        const {password, ...othersData} = user._doc;
+
+        res.cookie('access_token', token,{
+            httpOnly: true,
+        }).status(200).json(othersData);
 
     } catch (err) {
         next(err);
